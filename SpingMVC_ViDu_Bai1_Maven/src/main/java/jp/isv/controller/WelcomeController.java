7829005d55b6f3c24fd97e8118ac0a82486518d2
Lucky <Lucky@DESKTOP-P8ICQDV>
2025-03-03 23:17:02 +0700

    Upload SpringMVC Code

---
+++ SpingMVC_ViDu_Bai1_Maven/src/main/java/jp/isv/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package jp.isv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "viewWelcome";
+	}
+}
