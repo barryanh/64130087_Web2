7160d33e58fb5b858e6d2b60f8c6100c6e106799
Lucky <Lucky@DESKTOP-P8ICQDV>
2025-03-18 09:07:05 +0700

    Upload LoginV2 Code

---
+++ LoginVemOniV2/src/main/java/vemoni/edu/LoginVemOniV2/LoginVemOniV2Application.java
@@ -0,0 +1,13 @@
+package vemoni.edu.LoginVemOniV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVemOniV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginVemOniV2Application.class, args);
+	}
+
+}
---
+++ LoginVemOniV2/src/test/java/vemoni/edu/LoginVemOniV2/LoginVemOniV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vemoni.edu.LoginVemOniV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginVemOniV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
