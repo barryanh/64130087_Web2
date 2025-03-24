66788dce72dc7b6518611a874ba85ff7796ebbd0
Lucky <Lucky@DESKTOP-P8ICQDV>
2025-03-24 22:22:02 +0700

    Upload LayOut Code

--- SB_Layout/src/main/java/vemoni/edu/SB_Layout/SbLayoutApplication.java
+++ SB_Layout/src/main/java/vemoni/edu/SB_Layout/SbLayoutApplication.java
@@ -1,13 +1,13 @@
-package vemoni.edu.LoginVemOniV2;
+package vemoni.edu.SB_Layout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginVemOniV2Application {
+public class SbLayoutApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginVemOniV2Application.class, args);
+		SpringApplication.run(SbLayoutApplication.class, args);
 	}
 
 }
--- SB_Layout/src/test/java/vemoni/edu/SB_Layout/SbLayoutApplicationTests.java
+++ SB_Layout/src/test/java/vemoni/edu/SB_Layout/SbLayoutApplicationTests.java
@@ -1,10 +1,10 @@
-package vemoni.edu.LoginVemOniV2;
+package vemoni.edu.SB_Layout;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginVemOniV2ApplicationTests {
+class SbLayoutApplicationTests {
 
 	@Test
 	void contextLoads() {
