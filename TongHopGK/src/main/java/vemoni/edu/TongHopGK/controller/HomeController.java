81b7b8516ca2af97ff5fba6c6d57306ed2df8444
Lucky <Lucky@DESKTOP-P8ICQDV>
2025-03-25 10:56:46 +0700

    Upload Code lần 2

--- TongHopGK/src/main/java/vemoni/edu/TongHopGK/controller/HomeController.java
+++ TongHopGK/src/main/java/vemoni/edu/TongHopGK/controller/HomeController.java
@@ -1,7 +1,13 @@
 package vemoni.edu.TongHopGK.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vemoni.edu.TongHopGK.model.Student;
 
 @Controller
 public class HomeController {
@@ -16,7 +22,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("/studentList")
-	public String studentList() {
+	public String toList(ModelMap model) {
+		ArrayList<Student> sinhViens = new ArrayList<Student>(Arrays.asList(
+				new Student("64130282", "Nguyễn Huy Đạt", 8.5),
+				new Student("64130087", "Trương Quốc Anh", 9.0),
+				new Student("64139504", "Nguyễn Vũ Hòa", 8.6)
+				));
+		model.addAttribute("svs", sinhViens);
 		return "studentList";
 	}
 }
