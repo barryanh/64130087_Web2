38027ec7a1dd97487689c4c30bc6d492c3431585
Lucky <Lucky@DESKTOP-P8ICQDV>
2025-04-01 10:39:02 +0700

    Update code

--- TruongQuocAnh_ThiGK/src/main/java/thiGK/ntu64130087/TruongQuocAnh_ThiGK/controller/HomeController.java
+++ TruongQuocAnh_ThiGK/src/main/java/thiGK/ntu64130087/TruongQuocAnh_ThiGK/controller/HomeController.java
@@ -4,11 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import thiGK.ntu64130087.TruongQuocAnh_ThiGK.service.StudentService;
+import thiGK.ntu64130087.TruongQuocAnh_ThiGK.service.TopicService;
+
 @Controller
 public class HomeController {
-	@GetMapping({"/", "/dashboard"})
+	private final StudentService studentService;
+    private final TopicService topicService;
+
+    public HomeController(StudentService studentService, TopicService topicService) {
+        this.studentService = studentService;
+        this.topicService = topicService;
+    }
+ 
+    @GetMapping({"/", "/dashboard"})
     public String dashboard(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        model.addAttribute("topics", topicService.getAllTopics());
+
         model.addAttribute("username", "Student Name");
+
         return "dashboard";
     }
 }
